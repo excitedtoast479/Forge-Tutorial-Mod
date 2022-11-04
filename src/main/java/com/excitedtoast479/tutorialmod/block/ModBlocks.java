6ef08328be8f695294e9f19cda1db6de60eafea9
excitedtoast479 <116467223+excitedtoast479@users.noreply.github.com>
2022-11-04 16:18:48 -0700

    Almost added medium vase and its models

--- src/main/java/com/excitedtoast479/tutorialmod/block/ModBlocks.java
+++ src/main/java/com/excitedtoast479/tutorialmod/block/ModBlocks.java
@@ -1,6 +1,7 @@
 package com.excitedtoast479.tutorialmod.block;
 
 import com.excitedtoast479.tutorialmod.TutorialMod;
+import com.excitedtoast479.tutorialmod.block.custom.MediumVaseBlock;
 import com.excitedtoast479.tutorialmod.item.ModCreativeModeTab;
 import com.excitedtoast479.tutorialmod.item.ModItems;
 import net.minecraft.util.valueproviders.UniformInt;
@@ -20,13 +21,9 @@ import java.util.function.Supplier;
 
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
-            DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
+                DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
 
-    public static final RegistryObject<Block> MYTHRIL_ORE = registerBlock("mythril_ore",
-            () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(2f).requiresCorrectToolForDrops(), UniformInt.of(1, 10)), ModCreativeModeTab.TUTORIAL_TAB);
-
-    public static final RegistryObject<Block> MEDIUM_VASE = registerBlock("med",
+        public static final RegistryObject<Block> MYTHRIL_ORE = registerBlock("mythril_ore",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
                     .strength(2f).requiresCorrectToolForDrops(), UniformInt.of(1, 10)), ModCreativeModeTab.TUTORIAL_TAB);
 
@@ -37,6 +34,10 @@ public class ModBlocks {
     public static final RegistryObject<Block> MYTHRIL_BLOCK = registerBlock("mythril_block",
             () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
                     .strength(2f).requiresCorrectToolForDrops()), ModCreativeModeTab.TUTORIAL_TAB);
+
+    public static final RegistryObject<Block> MEDIUM_VASE = registerBlock("medium_vase",
+            () -> new MediumVaseBlock(BlockBehaviour.Properties.of(Material.METAL)
+                    .strength(2f).requiresCorrectToolForDrops().noOcclusion()), ModCreativeModeTab.TUTORIAL_TAB);
 
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
